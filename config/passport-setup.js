621a59e2629f9acae82fa96a24bca4a812c8beab
michael tamsil <michaeltamsil@gmail.com>
2018-09-28 15:09:15 +0700

    lesson: episod 13 need to continue

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
+const User = require('../models/user-model')
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +10,21 @@ passport.use(
         clientSecret: keys.google.clientSecret
         // options for the google strat
     },(accessToken, refreshToken, profile, done) => {
-        console.log('passport callback function fired')
-        console.log(profile)
+        // check if user already exist in our database
+        User.findOne({
+            googleId: profile.id
+        }).then((currentUser) => {
+            if(currentUser){
+                // already have the user
+            }
+        })
+
+        new User({
+            username: profile.displayName,
+            googleId: profile.id
+        }).save().then( newUser => {
+            console.log(`new user created ${newUser}`)
+        })
         // passport callback function
     })
 )
